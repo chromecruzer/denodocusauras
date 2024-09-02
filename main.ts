149907c95cadc4fb4c8891faad0aac2bfe6bce0a
chromecruzer <147481419+chromecruzer@users.noreply.github.com>
2024-09-02 21:00:19 +0530

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 // Importing Node.js modules
 import express, { Request, Response } from 'express';
 import compression from 'compression';
-import path from 'path';
+import { join, cwd } from "https://deno.land/std@0.190.0/path/mod.ts";
 
 // Server configurations
 const app = express();
@@ -10,8 +10,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Front-end client
-const clientBuildPath = path.join(process.cwd(), 'build');
-console.log(clientBuildPath.bgYellow);
+const clientBuildPath = join(cwd(), 'build');
+console.log(clientBuildPath);
 
 // Serve static files from the build directory
 app.use(express.static(clientBuildPath));
@@ -21,16 +21,16 @@ app.get('*', (req: Request, res: Response, next: () => any) => {
   if (req.path.startsWith('/api')) {
     return next();
   }
-  res.sendFile(path.join(clientBuildPath, 'index.html'));
+  res.sendFile(join(clientBuildPath, 'index.html'));
 });
 
 // API route
 app.get('/api', async (_req: Request, res: Response) => {
-  await res.status(200).json({ msg: 'This data comes from Backend Node.js Server' });
+  await res.status(200).json({ msg: 'This data comes from Backend Deno Server' });
 });
 
 // Port configuration
-const port = process.env.PORT || 3000;
+const port = parseInt(Deno.env.get("PORT") || "3000", 10);
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`.bgGreen.bold);
+  console.log(`Server is running at http://localhost:${port}`);
 });
